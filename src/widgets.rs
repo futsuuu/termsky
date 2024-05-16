1435547964c33ec9f70d7def090c1b12d2a9f693
futsuuu <futsuuu123@gmail.com>
2024-05-16 19:02:13 +0900

    refactor(widget): move `RectExt` to rect_ext.rs

--- src/widgets.rs
+++ src/widgets.rs
@@ -3,36 +3,12 @@ pub mod molecules;
 pub mod organisms;
 pub mod pages;
 mod post;
+mod rect_ext;
 mod store;
 pub mod templates;
 mod view;
 
 pub use post::{Posts, PostsState};
+pub use rect_ext::RectExt;
 use store::{Store, Storeable};
 pub use view::View;
-
-pub trait RectExt {
-    fn x(self, n: u16) -> Self;
-    fn y(self, n: u16) -> Self;
-    fn width(self, n: u16) -> Self;
-    fn height(self, n: u16) -> Self;
-}
-
-impl RectExt for ratatui::layout::Rect {
-    fn x(mut self, n: u16) -> Self {
-        self.x = n;
-        self
-    }
-    fn y(mut self, n: u16) -> Self {
-        self.y = n;
-        self
-    }
-    fn width(mut self, n: u16) -> Self {
-        self.width = n;
-        self
-    }
-    fn height(mut self, n: u16) -> Self {
-        self.height = n;
-        self
-    }
-}
---
+++ src/widgets/rect_ext.rs
@@ -0,0 +1,42 @@
+pub trait RectExt {
+    fn x(self, n: impl NewValue<u16>) -> Self;
+    fn y(self, n: impl NewValue<u16>) -> Self;
+    fn width(self, n: impl NewValue<u16>) -> Self;
+    fn height(self, n: impl NewValue<u16>) -> Self;
+}
+
+impl RectExt for ratatui::layout::Rect {
+    fn x(mut self, n: impl NewValue<u16>) -> Self {
+        self.x = n.new_value(self.x);
+        self
+    }
+    fn y(mut self, n: impl NewValue<u16>) -> Self {
+        self.y = n.new_value(self.y);
+        self
+    }
+    fn width(mut self, n: impl NewValue<u16>) -> Self {
+        self.width = n.new_value(self.width);
+        self
+    }
+    fn height(mut self, n: impl NewValue<u16>) -> Self {
+        self.height = n.new_value(self.height);
+        self
+    }
+}
+
+
+pub trait NewValue<T> {
+    fn new_value(self, old: T) -> T;
+}
+
+impl NewValue<u16> for u16 {
+    fn new_value(self, _: u16) -> u16 {
+        self
+    }
+}
+
+impl<F: FnOnce(u16) -> u16> NewValue<u16> for F {
+    fn new_value(self, old: u16) -> u16 {
+        self(old)
+    }
+}
